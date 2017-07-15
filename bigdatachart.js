a9c880371767e1f016161c32ec5e534a6b0592f5
June <luojun_nbu@hotmail.com>
2017-07-15 13:00:36 +0800

    Add multi line to one chart
    add the line of realTimeTotalReceivedBytesThreshold and averageTotalReceivedBytesThreshold to chartRD and chartPN

--- bigdatachart.js
+++ bigdatachart.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 
 	//横坐标（时间）
 	var date = [];
-
+    var dataBT=[];
+	var dataEBT=[];
 	//realTimeTotalReceivedBytes
 	var dataRD = [];
 	var optionRD = {
@@ -76,6 +77,54 @@ $(document).ready(function(){
 					}
 				},
 				data: dataRD
+			},
+			{
+				name:'模拟数据',
+				type:'line',
+				smooth:true,
+				symbol: 'none',
+				sampling: 'average',
+				itemStyle: {
+					normal: {
+						color: 'rgb(255, 70, 131)'
+					}
+				},
+				areaStyle: {
+					normal: {
+						color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+							offset: 0,
+							color: 'rgb(255, 158, 68)'
+						}, {
+							offset: 1,
+							color: 'rgb(255, 70, 131)'
+						}])
+					}
+				},
+				data: dataBT
+			},
+			{
+				name:'模拟数据',
+				type:'line',
+				smooth:true,
+				symbol: 'none',
+				sampling: 'average',
+				itemStyle: {
+					normal: {
+						color: 'rgb(255, 70, 131)'
+					}
+				},
+				areaStyle: {
+					normal: {
+						color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+							offset: 0,
+							color: 'rgb(255, 158, 68)'
+						}, {
+							offset: 1,
+							color: 'rgb(255, 70, 131)'
+						}])
+					}
+				},
+				data: dataEBT
 			}
 		]
 	};
@@ -154,61 +203,7 @@ $(document).ready(function(){
 					}
 				},
 				data: dataPN
-			}
-		]
-	};
-	var myChartPN = echarts.init(document.getElementById('echartPN'));
-	myChartPN.setOption(optionPN);
-
-	//realTimeTotalReceivedBytesThreshold
-	var dataBT = [];	
-	var optionBT = {
-		tooltip: {
-			trigger: 'axis',
-			position: function (pt) {
-				return [pt[0], '10%'];
-			}
-		},
-		title: {
-			left: 'center',
-			text: 'Real Time Total Received Bytes Threshold',
-		},
-		toolbox: {
-			feature: {
-				dataZoom: {
-					yAxisIndex: 'none'
-				},
-				restore: {},
-				saveAsImage: {}
-			}
-		},
-		xAxis: {
-			type: 'category',
-			boundaryGap: false,
-			data: date
-		},
-		yAxis: {
-			type: 'value',
-			boundaryGap: [0, '100%']
-		},
-		dataZoom: [{
-			type: 'inside',
-		   // start: 0,
-			//end: 100
-		}, {
-			start: 0,
-			end: 10,
-			handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-			handleSize: '80%',
-			handleStyle: {
-				color: '#fff',
-				shadowBlur: 3,
-				shadowColor: 'rgba(0, 0, 0, 0.6)',
-				shadowOffsetX: 2,
-				shadowOffsetY: 2
-			}
-		}],
-		series: [
+			},
 			{
 				name:'模拟数据',
 				type:'line',
@@ -232,61 +227,7 @@ $(document).ready(function(){
 					}
 				},
 				data: dataBT
-			}
-		]
-	};
-	var myChartBT = echarts.init(document.getElementById('echartBT'));
-	myChartBT.setOption(optionBT);
-
-	//averageTotalReceivedBytesThreshold
-	var dataEBT = [];			
-	var optionEBT = {
-		tooltip: {
-			trigger: 'axis',
-			position: function (pt) {
-				return [pt[0], '10%'];
-			}
-		},
-		title: {
-			left: 'center',
-			text: 'Average Total Received Bytes Threshold',
-		},
-		toolbox: {
-			feature: {
-				dataZoom: {
-					yAxisIndex: 'none'
-				},
-				restore: {},
-				saveAsImage: {}
-			}
-		},
-		xAxis: {
-			type: 'category',
-			boundaryGap: false,
-			data: date
-		},
-		yAxis: {
-			type: 'value',
-			boundaryGap: [0, '100%']
-		},
-		dataZoom: [{
-			type: 'inside',
-		   // start: 0,
-			//end: 100
-		}, {
-			start: 0,
-			end: 10,
-			handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-			handleSize: '80%',
-			handleStyle: {
-				color: '#fff',
-				shadowBlur: 3,
-				shadowColor: 'rgba(0, 0, 0, 0.6)',
-				shadowOffsetX: 2,
-				shadowOffsetY: 2
-			}
-		}],
-		series: [
+			},
 			{
 				name:'模拟数据',
 				type:'line',
@@ -312,12 +253,10 @@ $(document).ready(function(){
 				data: dataEBT
 			}
 		]
-	};		
-	var myChartEBT = echarts.init(document.getElementById('echartEBT'));
-	myChartEBT.setOption(optionEBT);
-
+	};
+	var myChartPN = echarts.init(document.getElementById('echartPN'));
+	myChartPN.setOption(optionPN);
 	//新图表在此添加!!!!
-
 	//暂停接收
 	var quit=false;
 	$("#quit").click(function(){
@@ -352,9 +291,7 @@ $(document).ready(function(){
 			//localStorage.setItem(getdata.strCurrentTime, getdata.realTimeTotalPacketNum);
 			if(!quit){
 				myChartRD.setOption(optionRD);
-				myChartPN.setOption(optionPN);
-				myChartBT.setOption(optionBT);	
-				myChartEBT.setOption(optionEBT);		
+				myChartPN.setOption(optionPN);		
 			}
 		})
 	}, 1000);	
